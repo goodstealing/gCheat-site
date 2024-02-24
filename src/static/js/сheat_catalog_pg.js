01cd40baf75d465a923efe939d92cc2ec39c0376
goodstealing <xxxvladoss@gmail.com>
2024-02-24 08:44:41 +0300

    js for cheat_catalog

---
+++ src/static/js/сheat_catalog_pg.js
@@ -0,0 +1,5 @@
+
+var myCarousel = new bootstrap.Carousel(document.getElementById('myCarousel'), {
+  interval: 5000, // Интервал между переключениями в миллисекундах (необязательно)
+  wrap: true // Зацикливание карусели
+});
